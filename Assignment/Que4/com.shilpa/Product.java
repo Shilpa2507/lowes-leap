1e6dcb073609fd41b379b49da46263264afb467f
Shilpa2507 <84316119+Shilpa2507@users.noreply.github.com>
2023-04-03 16:55:35 +0530

    Update Product.java

--- Assignment/Que4/com.shilpa/Product.java
+++ Assignment/Que4/com.shilpa/Product.java
@@ -1,1 +1,22 @@
+package com.shilpa;
 
+public class Product 
+{
+	String pName;
+	//setter method
+//	public void setpName(String pName) {
+//	this.pName = pName;
+//}
+	
+	public void show()
+	{
+		System.out.println("Thanks for purchasing "+pName);
+	}
+	//using constructor
+	public Product(String pName) {
+		this.pName = pName;
+	}
+
+
+	
+}
